6ffd178a488cdd8671b87b670d3d80a20ca981ba
Shelf0 <shelffo4@gmail.com>
2021-06-24 20:34:58 -0400

    loop also finds out who has the max popular vote

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -32,3 +32,18 @@ with open(csv_filepath) as csvfile:
         if (v > max_popular_votes) :
             max_popular_votes = v
             winner = k
+
+
+print('Election Results')
+print('-' * 25)
+print(f'Total Votes: {total_votes}')
+print('-' * 25)
+# https://docs.python.org/3/library/string.html#format-string-syntax
+# This is what I'm using to format these values down below 
+print("Khan: {:.3f}% ({})".format(round(candidate_dictionary["Khan"]/total_votes * 100, 3), candidate_dictionary["Khan"]))
+print("Correy: {:.3f}% ({})".format(round(candidate_dictionary["Correy"]/total_votes * 100, 3), candidate_dictionary["Correy"]))
+print("Li: {:.3f}% ({})".format(round(candidate_dictionary["Li"]/total_votes * 100, 3), candidate_dictionary["Li"]))
+print("O'Tooley: {:.3f}% ({})".format(round(candidate_dictionary["O'Tooley"]/total_votes * 100, 3), candidate_dictionary["O'Tooley"]))
+print('-' * 25)
+print(f'Winner: {winner}')
+print('-' * 25)
