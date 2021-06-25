528a8eb6725c5a79ce47920b0d906235c4d5b05e
Shelf0 <shelffo4@gmail.com>
2021-06-24 20:22:11 -0400

    open & Use CSV

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,3 +9,9 @@ candidate_dictionary = {}
 total_votes = 0
 max_popular_votes = 0
 winner = ""
+
+# Open & Use CSV
+
+with open(csv_filepath) as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)
