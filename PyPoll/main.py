fb122230c8395db16afd7c6b2fd1300126ad0e50
Shelf0 <shelffo4@gmail.com>
2021-06-24 20:25:42 -0400

    used each candidates name as a key to attach values

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -15,3 +15,11 @@ winner = ""
 with open(csv_filepath) as csvfile:
     reader = csv.reader(csvfile)
     next(reader)
+
+    for row in reader:
+        total_votes += 1
+        candidates_name = row[2]
+        # This uses each candidates name as a key to attach values
+        # If the candidates name is present on the candidate_dictionary, it will increment the corresponding value by 1
+        
+        candidate_dictionary[candidates_name] = candidate_dictionary.get(candidates_name, 0) + 1
